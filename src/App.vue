0aa891ee1a3484323daa5f867ebcd9b04a9263cd
danranvm <danranvm@gmail.com>
2021-12-13 22:02:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <IxDrawerProvider ref="drawerProviderRef">
+    <IxModalProvider ref="modalProviderRef">
+      <IxNotificationProvider>
+        <IxMessageProvider>
+          <HelloWorld />
+        </IxMessageProvider>
+      </IxNotificationProvider>
+    </IxModalProvider>
+  </IxDrawerProvider>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { version } from "@idux/components/version";
+import { useMessage } from "@idux/components/message";
+
+const { info } = useMessage();
+const onClick = () => info("Hello @idux");
+</script>
+
+<template>
+  <h1>Hello @idux: {{ version }}</h1>
+  <p>
+    <IxButton mode="primary" @click="onClick">Say Hello</IxButton>
+  </p>
+  <p>
+    See
+    <a href="https://idux.site" target="_blank"> idux.site </a>
+    for more information.
+  </p>
+</template>
+
+<style scoped>
+p {
+  margin: 1em;
+}
+a {
+  color: #1c6eff;
+}
+</style>
